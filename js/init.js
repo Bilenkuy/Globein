aec8c4f23136da373a6fa7181de2b663ba246e0e
Степан <bilenkuy7440@gmil.com>
2021-11-02 23:41:31 +0200

    header

---
+++ js/init.js
@@ -0,0 +1,15 @@
+$ = jQuery
+
+
+// let time = 0
+// setInterval(()=> {
+//     console.log(time++);
+// }, 1000)
+$( document ).ready(function() {
+    $( ".burger-icon" ).click(function() {
+        $(this).toggleClass('active')
+      })
+})
+
+
+
