831cf697a94ea12b5b3d09019450a291769d085c
Степан <bilenkuy7440@gmil.com>
2021-11-25 14:17:51 +0200

    mission

--- js/init.js
+++ js/init.js
@@ -1,15 +1,14 @@
 $ = jQuery
 
 
-// let time = 0
-// setInterval(()=> {
-//     console.log(time++);
-// }, 1000)
+ let time = 0
+ setInterval(()=> {
+     console.log(time++);
+ }, 1000)
 $( document ).ready(function() {
     $( ".burger-icon" ).click(function() {
         $(this).toggleClass('active')
+     
+     
       })
 })
-
-
-
